9a09322ff5ec62e2637f321405185b31a21e65f3
yechu <yechukim217@gmail.com>
2022-05-15 00:05:48 +0900

    feat: add model, view, octopus 🐙

---
+++ JS Design Pattern/01_changing_expectations/ud989-retain/js/changed_retain.js
@@ -0,0 +1,62 @@
+const $ = (elem) => document.querySelector(elem);
+
+const model = {
+	init: function () {
+		if (!localStorage.notes) {
+			localStorage.notes = JSON.stringify([]);
+		}
+	},
+	add: function (obj) {
+		const data = JSON.parse(localStorage.notes);
+		data.push(obj);
+		localStorage.notes = JSON.stringify(data);
+	},
+	getAllNotes: function () {
+		return JSON.parse(localStorage.notes);
+	},
+};
+
+const octopus = {
+	addNewNote: function (noteStr) {
+		model.add({
+			content: noteStr,
+			date: Date.now(),
+		});
+		view.render();
+	},
+	getNotes: function () {
+		return model.getAllNotes().reverse();
+	},
+
+	init: function () {
+		model.init();
+		view.init();
+	},
+};
+//submit event는 input에 하면 x,
+//form에다 달아야됨
+const view = {
+	init: function () {
+		this.noteList = $('#notes');
+		const newNoteForm = $('#new-note-form');
+		const newNoteContent = $('#new-note-content');
+		newNoteForm.addEventListener('submit', (e) => {
+			e.preventDefault();
+			const entered = newNoteContent.value;
+			octopus.addNewNote(entered);
+			newNoteContent.value = '';
+			view.render();
+		});
+	},
+	render: function () {
+		let newNote = '';
+		octopus.getNotes().map((note) => {
+      newNote +=
+        `<li class="note">${note.content}
+        <span class="date">${new Date(note.date).toLocaleDateString()}</span></li>`;
+		});
+		this.noteList.innerHTML = newNote;
+	},
+};
+
+octopus.init();
---
+++ JS Design Pattern/01_changing_expectations/ud989-retain/js/retain.js
@@ -0,0 +1,62 @@
+$(function () {
+	const model = {
+		init: function () {
+			if (!localStorage.notes) {
+				localStorage.notes = JSON.stringify([]);
+			}
+		},
+		add: function (obj) {
+			const data = JSON.parse(localStorage.notes);
+			data.push(obj);
+			localStorage.notes = JSON.stringify(data);
+		},
+		getAllNotes: function () {
+			return JSON.parse(localStorage.notes);
+		},
+	};
+
+	const octopus = {
+		addNewNote: function (noteStr) {
+			model.add({
+				content: noteStr,
+			});
+			view.render();
+		},
+
+		getNotes: function () {
+			return model.getAllNotes();
+		},
+
+		init: function () {
+			model.init();
+			view.init();
+		},
+	};
+
+	const view = {
+		init: function () {
+			this.noteList = $('#notes');
+			const newNoteForm = $('#new-note-form');
+			const newNoteContent = $('#new-note-content');
+			newNoteForm.addEventListener('submit', (e) => {
+				console.log(newNoteContent);
+				octopus.addNewNote(newNoteContent.value);
+			});
+			newNoteForm.submit(function (e) {
+				octopus.addNewNote(newNoteContent.val());
+				newNoteContent.val('');
+				e.preventDefault();
+			});
+			view.render();
+		},
+		render: function () {
+			const htmlStr = '';
+			octopus.getNotes().forEach(function (note) {
+				htmlStr += '<li class="note">' + note.content + '</li>';
+			});
+			this.noteList.html(htmlStr);
+		},
+	};
+
+	octopus.init();
+});
